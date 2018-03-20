04e26a7ef6ba7b132ce7dcc26f1c2a534fa3222d
Yuai007 <mycheck007@hotmail.com>
2018-03-21 02:19:39 +0800

    fixed admin

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@
 //});
 Route::any( 'admin/login','Admin\LoginController@login' );
 Route::get( 'admin/code','Admin\LoginController@code' );
-//Route::get( 'admin/crypt','Admin\LoginController@crypt' );
+Route::get( 'admin/crypt','Admin\LoginController@crypt' );
 
 
 
