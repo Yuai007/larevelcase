949e3125aaff1dc7d983dc4a7647eb6193b7449f
mycheck007 <mycheck007@hotmail.com>
2018-03-21 17:47:37 +0800

    test

--- public/test.php
+++ public/test.php
@@ -5,11 +5,9 @@
  * Date: 2018/2/28
  * Time: 上午2:27
  */
-
-    $redis = new Redis();
-    $redis->connect("127.0.0.1","6379");  //php客户端设置的ip及端口
-    //存储一个 值
-    $redis->set("say","hello world");
-    echo $redis->get("say");     //应输出hello world
-
-
+$redis = new Redis();
+$redis->connect("193.112.60.146","6379");  //php客户端设置的ip及端口
+$redis->auth('aAa741734224');
+//存储一个 值
+$redis->set("say","hello world");
+echo $redis->get("say");     //应输出hello world
